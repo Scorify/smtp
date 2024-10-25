05fd6bdf62ae2dba109db555b95156b910188708
1nv8rZim <msf9542@rit.edu>
2024-10-24 22:59:05 -0400

    implement schema

--- main.go
+++ main.go
@@ -7,8 +7,14 @@ import (
 )
 
 type Schema struct {
-	Target string `key:"target"`
-	Port   int    `key:"port"`
+	Server    string `key:"server"`
+	Port      int    `key:"port" default:"25"`
+	Username  string `key:"username"`
+	Password  string `key:"password"`
+	Sender    string `key:"sender"`
+	Recipient string `key:"recipient"`
+	Body      string `key:"body"`
+	Secure    bool   `key:"secure"`
 }
 
 func Validate(config string) error {
